9b3b3fe92f21050203255f5ea4c2db7d33939b2b
adsoftsito-phd <120610355+adsoftsito-phd@users.noreply.github.com>
2023-04-26 15:46:30 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const server = require('./server');
 
 const PORT = process.env.PORT || 8080;
 
-server.listen(PORT, ()=> console.log('Server is live at localhost :'+ PORT))
+server.listen(PORT, ()=> console.log('Server is live at okteto :'+ PORT))
 
 
