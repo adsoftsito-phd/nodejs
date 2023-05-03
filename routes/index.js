3ac66d157def4a4c1538a25c4c209421f4bc3d88
adsoftsito <adsoft@live.com.mx>
2023-05-03 18:29:38 -0500

    add jwt

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,17 @@
 const { Router } = require('express');
 const controllers = require('../controllers');
+const userAuth = require('../middleware/userAuth')
+
 
 const router = Router();
 
 router.get('/', (req, res) => res.send('Welcome'));
 
-router.post('/users', controllers.createUser);
-router.get('/users', controllers.getAllUsers);
+router.post('/users', userAuth.saveUser,  controllers.createUser);
+router.get('/users', userAuth.validateToken,  controllers.getAllUsers);
+
+//login route
+router.post('/login', controllers.login )
 
 module.exports = router;
 
